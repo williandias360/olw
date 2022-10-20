316260bd007ed8594a319f6a593fbaafe8dd14c5
Willian D. Brito <willian.dias360@gmail.com>
2022-10-20 15:39:48 -0300

    front-end

--- app/Http/Controllers/BeerController.php
+++ app/Http/Controllers/BeerController.php
@@ -2,18 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
 use App\Jobs\ExportJob;
 use App\Jobs\SendExportEmailJob;
+use App\Jobs\StoreExportDataJob;
 use App\Services\PunkApiService;
 use App\Http\Requests\BeerRequest;
-use App\Jobs\StoreExportDataJob;
 use Illuminate\Support\Facades\Auth;
 
 class BeerController extends Controller
 {
     public function index(BeerRequest $request, PunkApiService $service)
     {
-        return $service->getBeers(...$request->validated());
+        $beers = $service->getBeers(...$request->validated());
+        return Inertia::render("Beers", [
+            "beers" => $beers
+        ]);
     }
 
     public function export(BeerRequest $request)
---
+++ resources/js/Pages/Beers.vue
@@ -0,0 +1,15 @@
+<template>
+    <AuthenticatedLayout>
+        <div class="py-12">
+            <template v-for="beer in beers" :key="beer.id">
+                <span>{{beer.name}}</span>
+            </template>
+        </div>
+    </AuthenticatedLayout>
+</template>
+
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue"
+defineProps(["beers"])
+</script>
+
